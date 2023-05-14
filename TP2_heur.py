f1aeb4874812e0799b50575fdad25c2324adf3ab
gustavo <gustavoandresfu@gmail.com>
2023-05-14 10:19:07 -0300

    TP2 final

--- TP2_heur.py
+++ TP2_heur.py
@@ -10,8 +10,8 @@ def bfs(grafo, nodo_inicial, objetivo):
         if nodo in visitado:
             continue
         visitado.add(nodo)
-       
-        print(" -> " +nodo  , end = ' ')  # Imprimo el nodo para conocer el avance
+
+        print(" -> " + str(nodo), end=' ')  # Imprimo el nodo para conocer el avance
         if nodo == objetivo:
             return nodo
 
@@ -21,36 +21,38 @@ def bfs(grafo, nodo_inicial, objetivo):
                 cola.append(vecino)
 
     return None
-def heuristica(distancia_estimada, cantidad_nodos):
-    return cantidad_nodos - (cantidad_nodos // distancia_estimada)
+
+def heuristica(distancia_minima, distancia_maxima):
+    return (distancia_maxima + distancia_minima) // 2
+
 
 grafo = {
-    "1": ["2"],
-    "2": ["1", "3"],
-    "3": ["2", "4"],
-    "4": ["3", "5"],
-    "5": ["4", "6"],
-    "6": ["5", "7"],
-    "7": ["6", "8"],
-    "8": ["7", "9"],
-    "9": ["8", "10"], #aqui pongo el objetivo a encontrar
-    "10": ["9"]
+    1: [2],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3, 5],
+    5: [4, 6],
+    6: [5, 7],
+    7: [6, 8],
+    8: [7, 9],
+    9: [8, 10], 
+    10: [9]
 }
 
-# de alguna manera tengo que fijar el objetivo
-objetivo = "4"  
-# la distancia estimada de  la posicion  desde el borde
-distancia_estimada = 4 
-#Elijo donde iniciar el recorrido la funcion heuristica
-# me dice donde me posiciono con posibilidades de mayor exito
 
-nodo_inicial = str(heuristica(2,len(grafo))) 
-print("Nodo de inicio: "+nodo_inicial)
+# la distancia estimada minima y maxima al objetivo
+distancia_minima = 6
+distancia_maxima = 8
 
-#corro la funcion bfs, desde el nodo elegido
-resultado = bfs(grafo, nodo_inicial, objetivo)
+#busco con la funcion el nodo ideal para iniciar busqueda
+nodo_inicial = heuristica(distancia_minima, distancia_maxima) 
 
-if resultado:
-    print(f"\n El objetivo fue encontrado en el nodo: {resultado}")
-else:
-    print("El objetivo no fue encontrado")
+for i in range(6, 9): #el rango corresponde a la ventana de movimiento del objetivo
+    objetivo = i # de alguna manera tengo que fijar el objetivo
+    print("Nodo de inicio: " + str(nodo_inicial))
+    # corro la función bfs, desde el nodo elegido
+    resultado = bfs(grafo, nodo_inicial, objetivo)
+    if resultado:
+        print(f"\nEl objetivo fue encontrado en el nodo: {resultado}")
+    else:
+        print("El objetivo no fue encontrado") 
