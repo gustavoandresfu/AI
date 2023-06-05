2e9861aebfdb8c9f9fafdf89051d83394590f370
gustavo <gustavoandresfu@gmail.com>
2023-06-05 06:41:59 -0300

    TP3

--- TP3/Hopfield_TP3_IMG.py
+++ TP3/Hopfield_TP3_IMG.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 # Función para cargar una imagen y convertirla en una matriz binaria
 def cargar_imagen_ruta(ruta_imagen):
     imagen = cv2.imread(ruta_imagen, 0)  # Cargar imagen en escala de grises
@@ -58,7 +59,7 @@ def identificar_patron(imagen, pesos):
     return n_estado
 
 # Ruta de la imagen de patrón  de entrada
-ruta_patron_entrada = "Original/imagen_motor.jpg"
+ruta_patron_entrada = "TP3/Original/imagen_motor.jpg"
 
 # Cargar imagen de patrón  de entrada y convertirla en una matriz
 patron_entrada = cargar_imagen_ruta(ruta_patron_entrada)
@@ -67,11 +68,11 @@ patron_entrada = cargar_imagen_ruta(ruta_patron_entrada)
 pesos = entrenar_hopfield(patron_entrada.flatten())
 
 # Ruta de la imagen de patrón desconocido
-ruta_patron_desconocido = "Muestras/imagen_zoom.jpg"
-#ruta_patron_desconocido = "Muestras/imagen_abajo_der.jpg"
-#ruta_patron_desconocido = "Muestras/imagen_arriba_izq.jpg"
-#ruta_patron_desconocido = "Muestras/imagen_perfiljpg"
-#ruta_patron_desconocido = "Muestras/imagen_zoom.jpg"
+ruta_patron_desconocido =  "TP3/Muestras/imagen_zoom.jpg"
+#ruta_patron_desconocido = "TP3/Muestras/imagen_abajo_der.jpg"
+#ruta_patron_desconocido = "TP3/Muestras/imagen_arriba_izq.jpg"
+#ruta_patron_desconocido = "TP3/Muestras/imagen_perfiljpg"
+#ruta_patron_desconocido = "TP3/Muestras/imagen_zoom.jpg"
 
 #Estas imagenes no se encuentra el patron:
 
